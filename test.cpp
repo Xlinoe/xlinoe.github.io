b0626111236187eab71cb8637a1d35441a96e2de
xlinoe <xlinoe111@gmail.com>
2023-03-05 22:37:02 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stdlib.h>
+#include<string>
+
+using namespace std;
+
+bool prsth(){
+    string pw = "mpquery";
+    string pws;
+    cin >> pws;
+    if(pw==pws){return true;}else{return false;};
+}
+
+int main(){
+    if(prsth()){
+        cout << "success!" << endl;
+    }else{
+        cout << "fail!" << endl;
+    };
+    system("pause");
+    return 0;
+};
